92475d875a0260a3830b826a36e732b298d10f8c
Hesam Mousavi <mousavi.hesam4321@gmail.comi>
2021-09-05 00:55:05 +0100

    3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use("/api/user", require("./routes/User"));
 app.use("/api/user/expenses", require("./routes/Expense"));
-app.use("/api/user/login", require("./routes/userLogin"));
+// app.use("/api/user/login", require("./routes/userLogin"));
 app.use("/api/user/resetpassword", require("./routes/resetPassword"));
 
 if (process.env.NODE_ENV === "production") {
