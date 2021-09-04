75a6707e7fab3741aa8700c8926186fc78114557
Hesam Mousavi <mousavi.hesam4321@gmail.comi>
2021-09-05 00:08:29 +0100

    fix

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use("/api/user", require("./routes/User"));
 app.use("/api/user/expenses", require("./routes/Expense"));
 app.use("/api/user/login", require("./routes/userLogin"));
 app.use("/api/user/resetpassword", require("./routes/resetPassword"));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("expendo/build"));
   app.get("*", (req, res) => {
