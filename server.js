a4c4cbc5743a5b850b23d9496cec63a7c728cb48
Hesam Mousavi <mousavi.hesam4321@gmail.comi>
2021-09-04 14:36:01 +0100

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+require("dotenve").config();
 const app = express();
 const connectDB = require("./db");
+const path = require("path");
 connectDB();
 const port = process.env.PORT || 5000;
+
 app.use("/api/user", require("./routes/User"));
 app.use("/api/user/expenses", require("./routes/Expense"));
 app.use("/api/user/login", require("./routes/userLogin"));
 app.use("/api/user/resetpassword", require("./routes/resetPassword"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("expendo/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "expendo", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`server is running in ${port}`);
